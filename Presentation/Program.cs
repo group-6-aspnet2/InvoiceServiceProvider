4de8b96295017ab690a48d3c839d6e04d0087da9
Arvid Berlin <arvidberlinjobb@gmail.com>
2025-05-25 19:40:28 +0200

    Testing CORS fixes

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -9,6 +9,19 @@ using Presentation.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(opts =>
+{
+    opts.AddPolicy("AllowFrontend", policy =>
+       policy
+        .WithOrigins(
+           "https://localhost:5173",
+           "https://invoiceserviceprovider-app.azurewebsites.net"
+        )
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+    );
+});
+
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddGrpc();
@@ -44,13 +57,12 @@ builder.Services.AddScoped<IUpdateBookingWithInvoiceIdHandler, UpdateBookingWith
 
 var app = builder.Build();
 
-app.MapOpenApi();
-app.UseHttpsRedirection();
-app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
-
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 app.MapGrpcService<InvoiceGrpcService>();
 
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.MapOpenApi();
+app.UseHttpsRedirection();
+app.UseCors("AllowFrontend");
 
 app.UseAuthentication();
 app.UseAuthorization();
