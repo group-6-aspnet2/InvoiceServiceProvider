fdeb122df92fc2caa313952d4a61bde2e62e5835
Arvid Berlin <arvidberlinjobb@gmail.com>
2025-05-26 18:45:31 +0200

    Revert "Added swagger"
    This reverts commit 825f564a50f8f34695c22016392b5a47e45e5396.

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -13,22 +13,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddGrpc();
-builder.Services.AddSwaggerGen();
 
+builder.WebHost.ConfigureKestrel(x =>
+{
+    x.ListenAnyIP(5299, listenOption =>
+    {
+        listenOption.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http1AndHttp2;
+    });
 
-//builder.WebHost.ConfigureKestrel(x =>
-//{
-//    x.ListenAnyIP(8585, listenOption =>
-//    {
-//        listenOption.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2;
-//    });
-
-//    x.ListenAnyIP(7239, listenOption =>
-//    {
-//        listenOption.UseHttps();
-//        listenOption.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http1AndHttp2;
-//    });
-//});
+    x.ListenAnyIP(7239, listenOption =>
+    {
+        listenOption.UseHttps();
+        listenOption.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http1AndHttp2;
+    });
+});
 
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
 
@@ -47,13 +45,6 @@ builder.Services.AddScoped<IUpdateBookingWithInvoiceIdHandler, UpdateBookingWith
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Invoice Service API V1");
-    c.RoutePrefix = string.Empty;
-});
-
 app.UseHttpsRedirection();
 app.UseRouting();
 app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
@@ -62,7 +53,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-app.MapGet("/grpc", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 app.MapGrpcService<InvoiceGrpcService>();
 app.MapOpenApi();
 
